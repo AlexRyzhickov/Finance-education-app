3d1c9cb0a732b2ab0842783e0e1b5e172b78a8f1
AlexRyzhickov <makseljoinb@gmail.com>
2021-05-20 13:49:40 +0300

    update goals fragment

--- app/src/main/java/com/atex/financeeducation/mainfragments/GoalsFragment.kt
+++ app/src/main/java/com/atex/financeeducation/mainfragments/GoalsFragment.kt
@@ -2,6 +2,7 @@ package com.atex.financeeducation.mainfragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -11,11 +12,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.atex.financeeducation.R
 import com.atex.financeeducation.adapters.GoalsAdapter
 import com.atex.financeeducation.adapters.SliderAdapter
-import com.atex.financeeducation.data.DreamItem
 import com.atex.financeeducation.data.GoalItem
 import com.atex.financeeducation.interfaces.ChangeBottomNavView
 import com.atex.financeeducation.viewmodel.CommonViewModel
-import com.example.androidkeyboardstatechecker.showToast
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.firestore.Query
@@ -46,6 +45,9 @@ class GoalsFragment : Fragment(R.layout.goals_fragment), SliderAdapter.OnItemCli
             isClickOpenAddGoal = true
         }
 
+        view.findViewById<TextView>(R.id.dreamName).text = "Мечта: ${args.dreamName}"
+        view.findViewById<TextView>(R.id.dreamDate).text = "Дата создания: ${args.createDate}"
+
         activity?.let {
             changeBotNavViewInterface = context as ChangeBottomNavView
             changeBotNavViewInterface.hideBottomNavView()
@@ -53,8 +55,8 @@ class GoalsFragment : Fragment(R.layout.goals_fragment), SliderAdapter.OnItemCli
 
         viewModel = ViewModelProvider(requireActivity()).get(CommonViewModel::class.java)
 
-        val query: Query = users.document("makseljoinb@gmail.com").collection("dreams")
-            .document("Airpods-04.05.2021-15:10:15").collection("goals")
+        val query: Query = users.document(viewModel.email).collection("dreams")
+            .document("${args.dreamName}-${args.createDate}-${args.createTime}").collection("goals")
         val options = FirestoreRecyclerOptions.Builder<GoalItem>()
             .setQuery(query, GoalItem::class.java)
             .build()
